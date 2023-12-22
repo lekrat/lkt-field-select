2875af4ca3188d344d249043fab1580108b9a3c5
Antonio Ibáñez <am.ibanez.costa@gmail.com>
2023-12-22 17:57:10 +0100

    Export Option type

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,5 +1,6 @@
 import { App } from 'vue';
 export { setNoOptionsMessage } from './functions/settings-functions';
+export type { Option } from "./types/Option";
 declare const LktFieldSelect: {
     install: (app: App) => void;
 };
--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,11 @@ import {default as selectField} from './lib-components/LktFieldSelect.vue';
 
 export {setNoOptionsMessage} from './functions/settings-functions';
 
+export type {Option} from "./types/Option";
+
 const LktFieldSelect = {
     install: (app: App) => {
         app.component('lkt-field-select', selectField);
     },
 };
-
 export default LktFieldSelect;

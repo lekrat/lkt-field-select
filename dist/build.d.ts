4ff1949b71b782d8906bf37f4454f0de72adb1a9
Antonio Ibáñez <am.ibanez.costa@gmail.com>
2024-02-27 11:24:27 +0100

    Update module types

---
+++ dist/build.d.ts
@@ -0,0 +1,7 @@
+declare namespace vr {
+    function install(e: any): void;
+}
+declare function pr(e: any): boolean;
+declare function hr(e: any, t: any): boolean;
+declare function _r(e: any, t: any): boolean;
+export { vr as default, pr as setNoOptionsMessage, hr as setResourceOptionSlot, _r as setResourceValueSlot };
